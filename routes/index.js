829a8a00b6be868ca6fd0859b1d36fbfadae6bdb
samirfaruki <samir.faruki@softobiz.com>
2022-11-01 15:21:09 +0530

    routing Completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard", messages: messages });
+});
+router.post("/new", function (req, res, next) {
+  // res.send("succefully");
+  res.redirect("/");
+});
 module.exports = router;
